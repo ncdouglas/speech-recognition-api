97a943436171f83ffb1d384aa62bb2420e42eab7
Natasha Douglas <ncdouglas2016@gmail.com>
2021-05-08 13:46:01 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMicrophone } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+    const commands = [
+      {
+        command: "open *",
+        callback: (website) => {
+          window.open("http://" + website.split(" ").join(""));
+        },
+      },
+      {
+        command: "change background color to *",
+        callback: (color) => {
+          document.body.style.background = color;
+        },
+      },
+      {
+        command: "reset",
+        callback: () => {
+          handleReset();
+        },
+      },
+      {
+        command: "reset background color",
+        callback: () => {
+          document.body.style.background = `rgba(0, 0, 0, 0.8)`;
+        },
+      },
+      {
+        command: "stop",
+        callback: () => {
+          stopHandle();
+        },
+      },
+    ];
+    const { transcript, resetTranscript } = useSpeechRecognition({ commands });
+    const [isListening, setIsListening] = useState(false);
+    const microphoneRef = useRef(null);
+    if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+      return (
+        <div className="mircophone-container">
+          Browser does not Support Speech Recognition.
+        </div>
+      );
+    }
+    const handleListing = () => {
+      setIsListening(true);
+      microphoneRef.current.classList.add("listening");
+      SpeechRecognition.startListening({
+        continuous: true,
+      });
+    };
+    const stopHandle = () => {
+      setIsListening(false);
+      microphoneRef.current.classList.remove("listening");
+      SpeechRecognition.stopListening();
+    };
+    const handleReset = () => {
+      stopHandle();
+      resetTranscript();
+    };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="microphone-wrapper">
+    <div className="mircophone-container">
+      <div className="microphone-icon-container"
+        ref={microphoneRef}
+        onClick={handleListing}
+      >
+        <FontAwesomeIcon icon={faMicrophone} className="microphone-icon"/>
+      </div>
+      <div className="microphone-status">
+        {isListening ? "Listening........." : "Click to start Listening"}
+      </div>
+      {isListening && (
+          <button className="microphone-stop btn" onClick={stopHandle}>
+            Stop
+          </button>
+        )}
     </div>
+
+    {transcript && (
+        <div className="microphone-result-container">
+          <div className="microphone-result-text">{transcript}</div>
+          <button className="microphone-reset btn" onClick={handleReset}>
+            Reset
+          </button>
+        </div>
+      )}
+
+  </div>
   );
 }
 
